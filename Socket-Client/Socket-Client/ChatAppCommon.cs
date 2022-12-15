897ca77c4aa7be446036c345f9c330600b965841
massu116 <kakeru1999116@yahoo.co.jp>
2022-12-15 17:32:22 +0900

    [add] ChatAppCommonクラスを追加

---
+++ Socket-Client/Socket-Client/ChatAppCommon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socket_Client {
+    /*
+    internal class ChatAppCommon {
+
+    }
+    */
+
+    //通信データクラス
+    public class CommunicationData {
+        //通信データサイズ最大値
+        const int MAX_COMMUNICATION_DATA_SIZE = 5120;
+
+        //通信データバッファ
+        public byte[] Data = new byte[MAX_COMMUNICATION_DATA_SIZE];
+
+        //通信クライアント
+        public TcpClientEx Client { get; private set; }
+
+        public CommunicationData(TcpClientEx info)
+        {
+            Client = info;
+        }
+
+        //通信データをUTF8でエンコーディングした文字列を取得する
+        public override string ToString()
+        {
+            return Encoding.UTF8.GetString(Data);
+        }
+    }
+
+    //Activeプロパティを外部から参照できるようにしたTcoListener拡張クラス
+    public class TcpListenerEx : TcpListener {
+        //  System.Net.Sockets.TcpListener がクライアント接続をアクティブに待機しているかどうかを示す値を取得します。
+        //  System.Net.Sockets.TcpListener がアクティブに待機している場合は true。それ以外の場合は false。
+
+        public new bool Active => base.Active;
+        public TcpListenerEx(IPEndPoint ep) : base(ep) { }
+    }
+
+    /// 接続中クライアントクラス。
+    /// TcpClientのSocketプロパティへのアクセスが面倒なのでアクセスを省略するための拡張クラス。
+
+    public class TcpClientEx {
+        public EndPoint RemoteEndPoint => Socket.RemoteEndPoint;
+        public TcpClient Client { get; private set; }
+        public Socket Socket => Client.Client;
+        public TcpClientEx(TcpClient client)
+        {
+            Client = client;
+        }
+
+        public TcpClientEx(IPEndPoint ep)
+        {
+            Client = new TcpClient(ep);
+        }
+    }
+}
